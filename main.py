54915a3c67e13b62a7098b4da361f6f2719adaa3
Aditya <lapratomo24@yahoo.com>
2024-08-13 16:11:58 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+from menu import Menu, MenuItem
+from coffee_maker import CoffeeMaker
+from money_machine import MoneyMachine
+
+# Using OOP to create the same output as Day 15
+
+menu = Menu()
+menu_item = MenuItem()
+coffee_maker = CoffeeMaker()
+money_machine = MoneyMachine()
